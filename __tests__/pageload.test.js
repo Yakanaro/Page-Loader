ddb1eba501390a66157a4e6c3cea5ec71ba3a742
Yakanaro <kipaherd@gmail.com>
2022-07-20 22:57:08 +0300

    step4

--- __tests__/pageload.test.js
+++ __tests__/pageload.test.js
@@ -1,63 +1,46 @@
 import nock from 'nock';
+import debug from 'debug';
 import { fileURLToPath } from 'url';
 import os from 'os';
 import path from 'path';
 import { promises as fs } from 'fs';
-import httpAdapter from 'axios/lib/adapters/http';
-import axios from 'axios';
 import pageLoader from '../src/pageload.js';
 
-axios.defaults.adapter = httpAdapter;
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const getFixturePath = (filename) => `${__dirname}/__fixtures__/${filename}`;
-
-nock.disableNetConnect();
-
-const link = 'https://hexlet.io/courses';
-const link2 = 'https://ru.hexlet.io/courses';
+const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 
 let expected;
 let distPath;
-let nockBody;
+let image;
 
 beforeEach(async () => {
   distPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  nockBody = await fs.readFile(getFixturePath('hexlet-io-courses.html'), 'utf-8');
-  expected = await fs.readFile(getFixturePath('expected.html'), 'utf-8');
+  expected = await fs.readFile(getFixturePath('expect.html'), 'utf-8');
+  // image = await fs.readFile(getFixturePath('img.svg'), 'utf-8');
 });
 beforeAll(async () => {
-  expected = await fs.readFile(getFixturePath('hexlet-io-courses.html'), 'utf-8');
+  image = await fs.readFile(getFixturePath('img.svg'), 'utf-8');
 });
 
-test('page-load', async () => {
-  nock('https://ru.hexlet.io').get('/courses').reply(200, expected);
-  await pageLoader(link2, distPath);
-  const files = await fs.readdir(distPath);
-  const resultFile = await fs.readFile(path.resolve(distPath, files[0]), 'utf-8');
+test('HTML-page with resources', async () => {
+  const scope = nock('https://ru.hexlet.io').get('/my').reply(200, expected).get('/img.svg').reply(200, image);
+  await pageLoader('https://ru.hexlet.io/my', distPath);
+  const page = await fs.lstat(path.join(distPath, 'ru-hexlet-io-my.html'));
+  const resourcesFolder = await fs.lstat(path.join(distPath, 'ru-hexlet-io-my_files'));
+  // const downloadedImg = await fs.lstat(path.join(distPath, 'ru-hexlet-io-my_files', 'img.svg'));
 
-  expect(resultFile).toEqual(expected);
+  expect(scope.isDone()).toBe(true);
+  expect(page.isFile()).toBe(true);
+  expect(resourcesFolder.isDirectory()).toBe(true);
+  // expect(downloadedImg.isFile()).toBe(true);
 });
 
-test('Successful page loading', async () => {
-  nock('https://hexlet.io').get('/courses').reply(200, nockBody);
-  nock('https://hexlet.io').get('/courses/file1.css').reply(200, 'Response from google');
-  nock('https://hexlet.io').get('/courses/file2.js').reply(200, 'Response from ebay');
+test('Should generate 404 error', async () => {
+  nock('https://abc.xyz').get('/a').reply(404);
+  await expect(pageLoader('https://abc.xyz/a', distPath)).rejects.toThrow('Request failed with status code 404');
+});
 
-  await pageLoader(link, distPath);
-
-  const htmlBeforeSavingResources = await fs.readFile(getFixturePath('hexlet-io-courses.html'));
-  const pageHtmlPath = path.join(distPath, 'hexlet-io-courses.html');
-  const loadedHtmlData = await fs.readFile(pageHtmlPath);
-
-  const sourceDirPath = path.join(distPath, 'hexlet-io-courses_files');
-  const sources = await fs.readdir(sourceDirPath);
-
-  expect(loadedHtmlData).toBeTruthy();
-  expect(loadedHtmlData).not.toBe(htmlBeforeSavingResources);
-
-  expect(sources.length).toBe(2);
-  expect(sources.includes('file1.css')).toBeTruthy();
-  expect(sources.includes('file2.js')).toBeTruthy();
+test('Should generate ENOENT error', async () => {
+  nock('https://example.com/').get('/a').reply(200, '<html></html');
+  await expect(pageLoader('https://example.com/a', path.join(distPath, 'any'))).rejects.toThrow('ENOENT');
 });
---
+++ src/utilites.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import { promises as fs } from 'fs';
+import path from 'path';
+import * as cheerio from 'cheerio';
+import debug from 'debug';
+import axiosDebug from 'axios-debug-log';
+
+axiosDebug({
+  request(deb, config) {
+    deb(`Request with ${config.headers['content-type']}`);
+  },
+  response(deb, response) {
+    deb(`Response with ${response.headers['content-type']}`, `from ${response.config.url}`);
+  },
+});
+
+const log = debug('page-loader:utils');
+
+const tags = {
+  script: 'src',
+  img: 'src',
+  link: 'href',
+};
+
+export const createFileName = (url) => {
+  const parts = url
+    .replace('https://', '')
+    .replace(/[\W]{1}$/g, '')
+    .replace(/[\W]/g, '-');
+  return parts;
+};
+
+export const checkLocalLink = (link, url) => {
+  const originalHost = new URL(url).origin;
+  return new URL(link, originalHost).origin === originalHost;
+};
+
+export const getFilesDirectoryPath = (url) => `${createFileName(url)}_files`;
+
+export const getFilename = (url) => {
+  const { pathname } = new URL(url, 'https://example.com');
+  const filename = pathname
+    .split('/')
+    .filter((el) => el !== '')
+    .join('-');
+  return filename === '' ? 'index.html' : filename;
+};
+
+export const downloadHtml = (url, htmlPath) => axios.get(url).then((response) => fs.writeFile(htmlPath, response.data, 'utf-8'));
+
+export const getLinksAndChangeHtml = (html, url) => {
+  log('parsing html for local links and transforming HTML-page');
+  const links = [];
+  const $ = cheerio.load(html);
+  Object.keys(tags).map((tag) =>
+    $(tag).each((i, el) => {
+      const link = $(el).attr(tags[tag]);
+      if (link && checkLocalLink(link, url)) {
+        $(el).attr(`${tags[tag]}`, `${path.join(getFilesDirectoryPath(url), getFilename(link))}`);
+        links.push(link);
+      }
+    })
+  );
+  return { links, newHtml: $.html() };
+};
+
+export const getAbsoluteUrls = (links, url) => {
+  return links.map((link) => new URL(link, url).href);
+};
+
+export const downloadResources = (links, resourcesPath) => {
+  log('downloading resources');
+  return fs.mkdir(resourcesPath).then(() => {
+    links.forEach((link) => {
+      axios({
+        method: 'get',
+        url: link,
+        responseType: 'arraybuffer',
+      }).then((data) => fs.writeFile(path.join(resourcesPath, getFilename(link)), data.data));
+    });
+  });
+};
