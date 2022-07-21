06f49e1a756e4e274c154bd551cb5ad41e145cf4
Yakanaro <kipaherd@gmail.com>
2022-07-21 23:13:31 +0300

    fix

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,37 +1,17 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { program } from 'commander';
 import pageLoader from '../src/pageload.js';
 
-const program = new Command();
-
 program
-  .version('1.0.0')
+  .version('0.1.0')
   .arguments('<url>')
-  .description('Page loader utility')
-  .option('-o, --output [dir]', 'output dir', process.cwd())
+  .description('web pages downloader')
+  .option('-o, --output [type]', 'destination', process.cwd())
   .action((url, options) => {
-    const outputPath = options.output;
-    pageLoader(url, outputPath)
-      .then(() => console.log('files have been created successfully'))
-      .catch((err) => {
-        console.error(err.message);
-        process.exit(1);
-      });
+    pageLoader(url, options.output).catch((err) => {
+      console.error(err.message);
+      process.exit(1);
+    });
   });
 program.parse(process.argv);
-
-// program
-//   .version('1.0.0')
-//   .description('Load page.')
-//   .option('-o, --output <pathToFolder>', 'folder', process.cwd())
-//   .arguments('<address>')
-//   .action((address) =>
-//     pageData(program.output, address)
-//       .then(() => console.log('files have been created successfully'))
-//       .catch((error) => {
-//         console.error(error.message);
-//         process.exit(1);
-//       })
-//   )
-//   .parse(process.argv);
