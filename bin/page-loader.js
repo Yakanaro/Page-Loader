bb7ce9dbe6292b8a58b3ed487a6f2ae20d7ea6e0
Yakanaro <kipaherd@gmail.com>
2022-07-24 14:22:50 +0300

    fix

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+// import { program } from 'commander';
 import pageLoader from '../src/pageload.js';
-import process from 'process';
+
+import { Command } from 'commander';
+
 // program
 //   .version('0.1.0')
 //   .arguments('<url>')
@@ -15,6 +17,21 @@ import process from 'process';
 //     });
 //   });
 // program.parse(process.argv);
+
+// program
+//   .version('0.1.0')
+//   .arguments('<url>')
+//   .description('web pages downloader')
+//   .option('-o, --output [type]', 'destination', process.cwd())
+//   .action((url, option) => {
+//     pageLoader(url, option.output).catch((err) => {
+//       console.error(err.message);
+//       process.exit(1);
+//     });
+//   });
+// program.parse(process.argv);
+
+const program = new Command();
 
 program
   .version('0.1.0')
