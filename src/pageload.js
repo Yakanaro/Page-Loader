76a2d540da87560d9ff2ad68e68aa1739d097a9e
Yakanaro <kipaherd@gmail.com>
2022-07-25 20:00:28 +0300

    fix

--- src/pageload.js
+++ src/pageload.js
@@ -1,10 +1,19 @@
 import { promises as fs } from 'fs';
 import path from 'path';
-import { createFileName, getFilesDirectoryPath, downloadHtml, getLinksAndChangeHtml, getAbsoluteUrls, downloadResources } from './utilites.js';
+import {
+  createFileName,
+  getFilesDirectoryPath,
+  downloadHtml,
+  getLinksAndChangeHtml,
+  getAbsoluteUrls,
+  downloadResources,
+  buildName,
+} from './utilites.js';
 
 const pageLoader = (url, destinationFolder = process.cwd()) => {
-  console.log(`${path.join(destinationFolder, createFileName(url))}.html`);
-  const htmlPath = `${path.join(destinationFolder, createFileName(url))}.html`;
+  console.log(`${path.join(destinationFolder, buildName(url))}.html`);
+  // const htmlPath = `${path.join(destinationFolder, createFileName(url))}.html`;
+  const htmlPath = `${path.join(destinationFolder, buildName(url))}.html`;
   const resourcesPath = path.join(destinationFolder, getFilesDirectoryPath(url));
   return downloadHtml(url, htmlPath)
     .then(() => fs.readFile(htmlPath, 'utf-8'))
